d926f405717fa62147246dcc5ca7b7021e09d2aa
Maingi0904 <immaculatemaingi33@gmail.com>
2025-09-21 21:24:26 +0300

    Add files via upload

---
+++ User input details program.c
@@ -0,0 +1,26 @@
+//C program
+ /*
+  Name:Immaculate Nyambura Maingi
+  Reg No:CT101/G/26509/25
+  Date:21/09/2025
+  Description:A program that prompts the user to input details
+ */
+#include<stdio.h>//Preprocessor directive scanf(),printf()
+//main function
+int main(){
+     int height;
+     float bankbalance;
+     int phonenumber;
+
+     printf("Enter your height:\n");
+     scanf("%d",&height);
+
+     printf("Enter bankbalance:\n");
+     scanf("%f",&bankbalance);
+
+     printf("Enter your phonenumber:\n");
+     scanf("%d",&phonenumber);
+
+     return 0;
+
+}
