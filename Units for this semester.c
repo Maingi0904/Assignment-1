7de6730209d258c69dee73febeb80835e6f09d24
Maingi0904 <immaculatemaingi33@gmail.com>
2025-09-21 21:23:41 +0300

    Add files via upload

---
+++ Units for this semester.c
@@ -0,0 +1,24 @@
+//Simple C program
+ /*
+  Name:Immaculate Nyambura Maingi
+  Reg No:CT101/G/26509/25
+  Date:21/09/2025
+  Description:Program displaying this semester  units
+ */
+ #include<stdio.h>
+ //main function
+ int main(){
+       printf("1.Analogue electronics\n");
+       printf("2.Mathematics for science\n");
+       printf("3.Discrete Mathematics\n");
+       printf("4.Introduction to computer Architecture\n");
+       printf("5.Fundamentals of ICT\n");
+       printf("6.Physics\n");
+       printf("7.Communication skills\n");
+       printf("8.Introduction to programming and problem-solving");
+
+       return 0;
+
+ }
+
+
