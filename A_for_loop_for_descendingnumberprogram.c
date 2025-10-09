2206ff3586f563bbfb0c74e9ca1889c861deb666
Maingi0904 <immaculatemaingi33@gmail.com>
2025-10-09 22:47:39 +0300

    Add files via upload

---
+++ A_for_loop_for_descendingnumberprogram.c
@@ -0,0 +1,18 @@
+/*
+Name:Immaculate Nyambura Maingi
+Reg No:CT101/G/26509/25
+Date:1/10/2025
+Description:A program for a for loop for descending numbers
+*/
+
+#include<stdio.h>
+
+int main(){
+
+    //for loop to print from 100 to 50
+    int i;
+     for (i =100;i >=50;i--){
+        printf("%d\n", i);
+        }
+    return 0;
+}
