8dcdea690e0d99f945a9b20119077151096d5004
Maingi0904 <immaculatemaingi33@gmail.com>
2025-10-09 23:08:27 +0300

    Add files via upload

---
+++ ATM_WITHDRAWALS.c
@@ -0,0 +1,35 @@
+/*
+Name:Immaculate  Nyambura Maingi
+Date:09/10/2025
+RegNO:CT101/G/26509/25
+Description:Withdrawal transcations program by ATM using while loop
+*/
+#include<stdio.h>
+int main(){
+    float amount_to_withdraw;
+    float initial_amount;
+    float balance;
+    printf("Enter initial amount:\n");
+    scanf("%f",&initial_amount);
+
+    printf("Enter amount to withdraw:\n");
+    scanf("%f",&amount_to_withdraw);
+
+
+
+
+    //compute
+    balance=initial_amount-amount_to_withdraw;
+//use of while loop
+ while(initial_amount>0){
+
+
+ }
+
+
+
+
+
+
+}
+
