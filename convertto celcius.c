67a62bfdd2c7c353a0f8fcb3b0824c869d842934
Maingi0904 <immaculatemaingi33@gmail.com>
2025-10-23 15:53:25 +0300

    Add files via upload

---
+++ convertto celcius.c
@@ -0,0 +1,29 @@
+
+/*
+Name:Immaculate  Nyambura Maingi
+Date:09/10/2025
+RegNO:CT101/G/26509/25
+Description:A c program that inputs temperature in fahrenheit and outputs temperature in celcius
+*/
+
+#include<stdio.h>
+
+float celcius(float F);
+
+void main(){
+    float result,F;
+    printf("Enter fahrenheit\n");
+    scanf("%f",&F);
+
+    //function call
+    result=celcius(F);
+    printf("The celcius is = %.2f\n",result);
+    return 0;
+}
+
+//function declaration
+float celcius(float F){
+    float C;
+    C=(F-32)*5/9;
+    return C;
+}
